21bf0f5362c49015ca706f4900b0316d816c4dea
Mina Fathy <minafathy2013738@gmail.com>
2025-04-13 19:37:44 +0200

    added Keil project , set up the layout and added the sourece and header files needed

---
+++ Keil_Project/Objects/lcd.d
@@ -0,0 +1,2 @@
+./objects/lcd.o: TM4C123_GH6PM_Drivers\LCD_Driver\LCD.c \
+  TM4C123_GH6PM_Drivers\LCD_Driver\LCD.h
---
+++ Keil_Project/Objects/main.d
@@ -0,0 +1,6 @@
+./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  TM4C123_GH6PM_Drivers\Inc\TM4C123GH6PM.h \
+  TM4C123_GH6PM_Drivers\Inc\TM4C123_GH6PM_GPIO.h \
+  TM4C123_GH6PM_Drivers\Inc\TM4C123_GH6PM_UART.h \
+  TM4C123_GH6PM_Drivers\GPS_Driver\GPS.h \
+  TM4C123_GH6PM_Drivers\LCD_Driver\LCD.h
