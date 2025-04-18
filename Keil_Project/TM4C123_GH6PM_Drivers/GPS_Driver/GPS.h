7f8aa6928e64d0aa360c89aac30d32d2d60e16a9
Mina Fathy <minafathy2013738@gmail.com>
2025-04-18 17:11:05 +0200

    added header protection to .h files and proper includes in .c and .h files

---
+++ Keil_Project/TM4C123_GH6PM_Drivers/GPS_Driver/GPS.h
@@ -0,0 +1,35 @@
+/*
+ * GPS.h
+ *
+ *  Created on: Apr 18, 2025
+ *    
+ */
+
+#ifndef INC_GPS_Driver_H_
+#define INC_GPS_Driver_H_
+
+//----------------------------
+//Includes
+//----------------------------
+
+#include "TM4C123GH6PM.h"
+#include "TM4C123_GH6PM_GPIO.h"
+#include "TM4C123_GH6PM_UART.h"
+#include "math.h"
+
+//----------------------------
+//Macros
+//----------------------------
+#define pi 3.1415926535
+
+
+//----------------------------
+//APIs
+//----------------------------
+
+//float_t GPS_Coords_To_DEG(float num);
+//float_t GPS_DEG_To_Rad(float num);
+
+
+
+#endif /* INC_GPS_Driver_H_ */
