ce49abe9570766e41471013b16a25d7d79f294ab
Mina Fathy <minafathy2013738@gmail.com>
2025-04-18 17:53:38 +0200

    added stuct for location and region and added Function APIs in GPS.h & 80% finished with GPS.c logic

--- Keil_Project/Objects/gps.d
+++ Keil_Project/Objects/gps.d
@@ -17,4 +17,5 @@
   TM4C123_GH6PM_Drivers\Inc\Utils.h \
   TM4C123_GH6PM_Drivers\Inc\TM4C123_GH6PM_UART.h \
   TM4C123_GH6PM_Drivers\Inc\TM4C123_GH6PM_GPIO.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  TM4C123_GH6PM_Drivers\LCD_Driver\LCD.h
