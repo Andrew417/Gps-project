ab790066ed5f5af98aa6df2a0e78d2f4bd989f59
EngArsany <arsanyo2003@gmail.com>
2025-05-06 00:01:28 +0300

    Clean GPIO.c code

--- Keil_Project/TM4C123_GH6PM_Drivers/TM4C123_GH6PM_GPIO.c
+++ Keil_Project/TM4C123_GH6PM_Drivers/TM4C123_GH6PM_GPIO.c
@@ -1,11 +1,8 @@
 #include "TM4C123_GH6PM_GPIO.h"
 void GPIO_init()
 {
-
-	// PA5, PA6, PA7, PB0, PB1, PB4, PD0, PD1, PD2, PE4, PE5
-	SYSCTL_RCGCGPIO_R |= 0x3F; // 00111111 for all ports
-	while ((SYSCTL_PRGPIO_R & 0x3F) == 0)
-		; // Wait until clock initialization for used GPIOs
+	SYSCTL_RCGCGPIO_R |= 0x3B; // 111011 for ports A, B, D & F
+	while ((SYSCTL_PRGPIO_R & 0x3B) == 0); // Wait until clock initialization for used GPIOs
 
 	/* ===TO USE ANY PORT N===
 	 * GPIO_PORTN_LOCK_R = GPIO_LOCK_KEY;    	// Unlock Port N commit register
@@ -26,64 +23,64 @@ void GPIO_init()
 	GPIO_PORTA_CR_R |= 0xE0;		   // The bits which equals 1 can be changed, 11100000 A5-7
 
 	// Analog is already disabled for Port A
-	GPIO_PORTA_PCTL_R &= ~0xE0;  // Clear PCTL bits for all pins
+	GPIO_PORTA_PCTL_R &= ~0xE0;  // Clear PCTL bits for selected pins
 	GPIO_PORTA_AFSEL_R &= ~0xE0; // Choose alternate functions on selected pins
-	GPIO_PORTA_DIR_R |= 0xE0;  // Set selected pins as outputs
+	GPIO_PORTA_DIR_R |= 0xE0;  // Set direction of selected pins: 0 input, 1 output
 	GPIO_PORTA_DEN_R = 0xE0;   // Enable digital function on selected pins:
-	GPIO_PORTA_DATA_R &= 0;	   // Initialize selected pins
+	GPIO_PORTA_DATA_R &= ~0xE0;	   // Initialize selected pins
 
 
 	// ==Port B==
 	//B3
 	GPIO_PORTB_LOCK_R = GPIO_LOCK_KEY; // Unlock Port B commit register
-	GPIO_PORTB_CR_R |= 0x08;		   // The bits which equals 1 can be changed, up to 8 bits N0->N7
+	GPIO_PORTB_CR_R |= 0x08;		   
 
 	GPIO_PORTB_AMSEL_R &= ~0x08;	  // Disable analog on selected pins
-	GPIO_PORTB_PCTL_R &= ~0x08;	  // Clear PCTL bits for selected pins
-	GPIO_PORTB_AFSEL_R &= ~0x08;	  // Choose alternate functions on selected pins
-	GPIO_PORTB_DIR_R |= 0x08;	  // Set direction of selected pins: 0 input, 1 output
-	GPIO_PORTB_DEN_R |= 0x08; // Enable digital function on selected pins
-	GPIO_PORTB_DATA_R = 0;	  // Initialize selected pins
+	GPIO_PORTB_PCTL_R &= ~0x08;	  
+	GPIO_PORTB_AFSEL_R &= ~0x08;	 
+	GPIO_PORTB_DIR_R |= 0x08;	  
+	GPIO_PORTB_DEN_R |= 0x08; 
+	GPIO_PORTB_DATA_R &= ~0x08;	  
 
 
 	// ==Port D==
 	//LCD: D1, D2, D3
 	//UART: D6, D7
 	GPIO_PORTD_LOCK_R = GPIO_LOCK_KEY; // Unlock Port D commit register
-	GPIO_PORTD_CR_R |= 0xCE;		   // Use needed the bits
+	GPIO_PORTD_CR_R |= 0xCE;		   // Use the needed bits
 
-	GPIO_PORTD_AMSEL_R &= ~0xCE;	  // Disable analog on selected pins
+	GPIO_PORTD_AMSEL_R &= ~0xCE;	  
 	GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & ~0xFF000000) | 0x11000000; // configure PD6, PD7 for UART2
 	GPIO_PORTD_PCTL_R &= ~0xCE;		// Clear PCTL for pins D1, D2, D3
 	GPIO_PORTD_AFSEL_R &= ~0x0E;	//Clear alternate function for D1, D2, D3
-	GPIO_PORTD_AFSEL_R |= 0xC0;	  // Choose alternate functions for D7, D6
+	GPIO_PORTD_AFSEL_R |= 0xC0;	  // Choose alternate functions for D7, D6 (UART)
 	GPIO_PORTD_DIR_R |= 0x8E;	  // Set output for pins D1, D2, D3, D7 (U2Tx)
 	GPIO_PORTD_DIR_R &= ~0x40; // PD6 (U2Rx) as input
-	GPIO_PORTD_DEN_R |= 0xCE; // Enable digital function on selected pins
-	GPIO_PORTD_DATA_R = 0;	  // Initialize all pins
+	GPIO_PORTD_DEN_R |= 0xCE; 
+	GPIO_PORTD_DATA_R &= ~0xCE;	  
 
 
 	// ==Port E==
 	// E5, E4, E1
-	GPIO_PORTE_LOCK_R = GPIO_LOCK_KEY; // Unlock Port D commit register
-	GPIO_PORTE_CR_R |= 0x30;		   // The bits which equals 1 can be changed, up to 8 bits N0->N7
+	GPIO_PORTE_LOCK_R = GPIO_LOCK_KEY; // Unlock Port E commit register
+	GPIO_PORTE_CR_R |= 0x32;		   
 
-	GPIO_PORTE_AMSEL_R &= ~0x32;	  // Disable analog on selected pins
-	GPIO_PORTE_PCTL_R &= ~0x32;	  // Clear PCTL bits for selected pins
-	GPIO_PORTE_AFSEL_R &= ~0x32;	  // Choose alternate functions on selected pins
-	GPIO_PORTE_DIR_R |= 0x32;	  // Set direction of selected pins: 0 input, 1 output
-	GPIO_PORTE_DEN_R |= 0x32; // Enable digital function on selected pins
-	GPIO_PORTE_DATA_R = 0;	  // Initialize selected pins
+	GPIO_PORTE_AMSEL_R &= ~0x32;	  
+	GPIO_PORTE_PCTL_R &= ~0x32;	  
+	GPIO_PORTE_AFSEL_R &= ~0x32;	  
+	GPIO_PORTE_DIR_R |= 0x32;
+	GPIO_PORTE_DEN_R |= 0x32; 
+	GPIO_PORTE_DATA_R &= ~0x32;	  
 
 
 	// ==Port F==
-	//F1, F2, F3
+	//F1, F2, F3 (LEDs)
 	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY; // Unlock Port F commit register
-	GPIO_PORTF_CR_R |= 0x0E; //allow changes to to PF321
+	GPIO_PORTF_CR_R |= 0x0E;
 
-	GPIO_PORTF_AMSEL_R &= ~0x0E;   //disable analog function
-	GPIO_PORTF_AFSEL_R &= ~0x0E;   // NO alternate function
-	GPIO_PORTF_PCTL_R &= ~0x0000FFF0; // GPIO Clear bit PCTL
+	GPIO_PORTF_AMSEL_R &= ~0x0E;   
+	GPIO_PORTF_AFSEL_R &= ~0x0E;   
+	GPIO_PORTF_PCTL_R &= ~0x0000FFF0;
 	GPIO_PORTF_DEN_R |= 0x0E;
 	GPIO_PORTF_DIR_R |= 0x0E;
 	//GPIO_PORTF_PUR_R |=0x10;
